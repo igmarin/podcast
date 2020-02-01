d683b37c46635a0dc9e572af1bd482e1fea24153
Ismael G Marin C <imarin@unitedvirtualities.com>
2020-02-01 14:23:04 -0600

    Relationships between Models

--- app/Models/Podcast.js
+++ app/Models/Podcast.js
@@ -1,9 +1,25 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Podcast extends Model {
+  podcaster() {
+    return this.belongsTo("App/Models/User");
+  }
+
+  episodes() {
+    return this.hasMany("App/Models/Episode");
+  }
+  category() {
+    return this.hasMany("App/Models/Category");
+  }
+
+  subscribers() {
+    return this.belongsToMany("App/Models/User")
+      .pivotTable("subscriptions")
+      .withTimestamps();
+  }
 }
 
-module.exports = Podcast
+module.exports = Podcast;
