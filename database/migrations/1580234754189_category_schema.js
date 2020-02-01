9ad23d051999512abadb8db68c5318165c89988d
Ismael G Marin C <imarin@unitedvirtualities.com>
2020-02-01 14:16:09 -0600

    Base migrations

---
+++ database/migrations/1580234754189_category_schema.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class CategorySchema extends Schema {
+  up() {
+    this.create("categories", table => {
+      table.increments();
+      table.string("name").notNullable();
+      table
+        .string("slug")
+        .notNullable()
+        .unique();
+      table.text("description");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("categories");
+  }
+}
+
+module.exports = CategorySchema;
---
+++ database/migrations/1580234773535_episode_schema.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class EpisodeSchema extends Schema {
+  up() {
+    this.create("episodes", table => {
+      table.increments();
+      table
+        .integer("podcast_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("podcasts")
+        .onDelete("cascade");
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable();
+      table
+        .string("title")
+        .notNullable()
+        .unique();
+      table
+        .string("slug")
+        .notNullable()
+        .unique();
+      table.text("summary").notNullable();
+      table.string("audio").notNullable();
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("episodes");
+  }
+}
+
+module.exports = EpisodeSchema;
