9ea4d36a48b291dfdef3e3ec4a8b64e71d16300f
Ismael G Marin C <imarin@unitedvirtualities.com>
2020-02-12 18:01:29 -0600

    Save podcast

--- app/Controllers/Http/PodcastController.js
+++ app/Controllers/Http/PodcastController.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const Category = use("App/Models/Category");
+const Podcast = use("App/Models/Podcast");
 const { validateAll } = use("Validator");
+const Helpers = use("Helpers");
+const uuid = require("uuid/v4");
 
 class PodcastController {
   async create({ view }) {
@@ -10,7 +13,7 @@ class PodcastController {
     return view.render("podcasts.create", { categories });
   }
 
-  async store({ request, response, session }) {
+  async store({ request, response, auth, session }) {
     const rules = {
       title: "required",
       category_id: "required",
@@ -23,6 +26,39 @@ class PodcastController {
 
       return response.redirect("back");
     }
+
+    const user = auth.user;
+    const logo = request.file("logo", {
+      types: ["image"],
+      size: "2mb"
+    });
+
+    await logo.move(Helpers.publicPath("uploads/logos"), {
+      name: `${uuid()}.${logo.subtype}`
+    });
+
+    if (!logo.moved()) {
+      session.flash({
+        type: "danger",
+        message: logo.error().message
+      });
+
+      return response.redirect("back");
+    }
+
+    const podcast = new Podcast();
+    podcast.title = request.input("title");
+    podcast.category_id = request.input("category_id");
+    podcast.description = request.input("description");
+    podcast.logo = `uploads/logos/${logo.fileName}`;
+    await user.podcast().save(podcast);
+
+    session.flash({
+      type: "success",
+      message: "Podcast created successfully"
+    });
+
+    return response.route("myPodcast");
   }
 }
 
